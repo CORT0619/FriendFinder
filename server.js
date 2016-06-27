24cc85611c506009a5b6dfbea9861828e2ca4796
Crystal <cortney0215@yahoo.com>
2016-06-26 20:30:47 -0400

    adding a few form elements to survey.html and making home.html pretty

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
 
 //app routes
-app.use(express.static('public'));
+app.use(express.static('app/public'));
 require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
 
