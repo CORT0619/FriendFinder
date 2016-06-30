693bd5a2ad65f7a2986649dcb941b1bfd5158d1c
Crystal <cortney0215@yahoo.com>
2016-06-30 15:14:11 -0400

    best match picture & name is now displaying properly in modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 exports.friends = [
 {
-	name: "",
-	photo: "",
+	name: "Henry",
+	photo: "http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg",
 	scores: [
      5,
      1,
